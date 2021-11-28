c45e1bf657475345645f20841ba84c9bc4ce61d5
Paul Spain <pvspain@gmail.com>
2021-11-28 17:53:16 +1100

    Lesson 3 Add divs to flesh out page Add image and anchor using v-bind: (shorthand ':')

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
 	data() {
 		return {
 			product: 'Socks',
-			description: 'You know - foot warmers'
+			description: 'You know - foot warmers',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://example.org'
 		}
 	}
 })
